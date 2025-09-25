23b6c2468e8340f60d29298f29d04e3b5e7ee677
Jonathan Gallo <jonathangallo2005@icloud.com>
2025-09-24 22:37:48 -0400

    FEAT - Added consistent header component, login/signup page

--- client/src/pages/line-chart.js
+++ client/src/pages/line-chart.js
@@ -1,3 +1,12 @@
+import Header from "../components/header";
+
 export default function lineChart() {
-  
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Line Chart</h1>
+      </main>
+    </>
+  );
 }
