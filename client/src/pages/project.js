c1e5a1dd948ce9bcbfe8a817e2dcb03d52c2d16a
Jonathan Gallo <jonathangallo2005@icloud.com>
2025-09-30 10:38:05 -0400

    [FEAT] - Created project.js placeholder page

---
+++ client/src/pages/project.js
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function Project() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Project</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
